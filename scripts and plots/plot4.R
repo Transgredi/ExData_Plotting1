938c5915571ef422b0046ed16dd79535c892d7f3
Transgredi <transgredi@gmail.com>
2014-08-06 21:16:12 +0200

    All graphs

--- scripts and plots/plot4.R
+++ scripts and plots/plot4.R
@@ -19,11 +19,22 @@ df$DateTime <- paste(df$Date, df$Time)
 df$DateTime <- strptime(df$DateTime, "%d/%m/%Y %H:%M:%S")
 df$Date <- as.Date(df$Date, "%d/%m/%Y")
 
+png("plot4.png")
 par(mfrow = c(2,2))
 
-png("plot4.png")
+#plot 1
+plot(df$DateTime, df$Global_active_power,
+  type="l",
+  ylab = "Global Active Power",
+  xlab = "")
 
+#plot 2
+plot(df$DateTime, df$Voltage,
+  type="l",
+  ylab = "Voltage",
+  xlab = "datetime")
 
+#plot 3
 plot(df$DateTime, df$Sub_metering_1,
   type="l",
   ylab = "Energy sub metering",
@@ -36,5 +47,11 @@ legend("topright",
   legend = names(df[ , c(7:9)]),
   lty=c(1, 1, 1),
   col=c("black", "red", "blue"))
+
+#plot 4
+plot(df$DateTime, df$Global_reactive_power,
+  type="l",
+  ylab = "Global reactive power",
+  xlab = "datetime")
 
 dev.off()
